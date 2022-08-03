192597b487a1486090b20b81801654e1e71e6085
shrey27 <shreypandey27@gmail.com>
2022-08-03 18:54:38 +0530

    firebase keys updated

--- src/frontend/firebase/firebase.js
+++ src/frontend/firebase/firebase.js
@@ -1,14 +1,14 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
-console.log(process.env.REACT_APP_API_KEY);
+
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
+  apiKey: "AIzaSyDsUx8D0hKtJWaFuJcCCfX8l8tkaLV66dE",
+  storageBucket: "neogcamp-ce308.appspot.com",
+  projectId: "neogcamp-ce308",
+  authDomain: "neogcamp-ce308.firebaseapp.com",
+  messagingSenderId: "436647397068",
+  appId: "1:436647397068:web:14e2d54344bcfe86a2a469",
 };
 
 // Initialize Firebase
